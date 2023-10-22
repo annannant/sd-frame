28eed46b6df65d2f3abd88bed77467658be06b4e
Barami Watthanakunanon <barami.wat@cpf.co.th>
2023-10-23 05:42:42 +0700

    del

--- cms/src/pages/production-orders/production-orders-info.tsx
+++
@@ -1,4 +0,0 @@
-
-export const ProductionOrdersInfo = () => {
-  return <>ProductionOrdersInfo</>
-}
--- cms/src/pages/production-orders/production-orders-list.tsx
+++
@@ -1,4 +0,0 @@
-
-export const ProductionOrdersList = () => {
-  return <>ProductionOrdersList</>
-}
